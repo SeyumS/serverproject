f865965b9bb5e24004dcd7649ecc91ec581b9cdd
SeyumS <20samuels02@gmail.com>
2025-04-28 18:26:41 +0200

    Add files via upload

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const {getAllTours, createTour,getTour,updateTour,deleteTour} = require('./../controllers/tourController');
+const {getAllTours, createTour,getTour,updateTour,deleteTour, checkID, checkBody} = require('./../controllers/tourController');
 
 const router = express.Router();
 
-
+router.param('id', checkID);
 
 router.route('/').get(getAllTours)
-                 .post(createTour);
+                 .post(checkBody, createTour);
                           
 
 router.route('/:id').get(getTour)
